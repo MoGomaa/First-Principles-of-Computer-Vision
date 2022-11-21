acf63d0de405158c5c9d86623fd8a72e9374689f
Mohamed Gomaa <m.gomaa@mcsoil.com>
2022-11-21 10:19:23 +0200

    fix typo, fix getConnectedComponent function call

--- 1 - Camera and Imaging/4.3 Segmenting Binary Images/Implementation.py
+++ 1 - Camera and Imaging/4.3 Segmenting Binary Images/Implementation.py
@@ -15,46 +15,46 @@ def fixEquivalenceTable(table, key, label):
         for otherLabel in table[key][:-1]:
             fixEquivalenceTable(table, otherLabel, label)
 
-def equalizeNormalizeLabeldImage(labeldImage, equivalenceTable):
+def equalizeNormalizeLabeledImage(labeledImage, equivalenceTable):
     Areas      = []
     Centeroids = []
     newTable   = {}
-    for r in range(labeldImage.shape[0]):
-        for c in range(labeldImage.shape[1]):
-            if labeldImage[r][c]:
-                label = getLabel(equivalenceTable, labeldImage[r][c])
+    for r in range(labeledImage.shape[0]):
+        for c in range(labeledImage.shape[1]):
+            if labeledImage[r][c]:
+                label = getLabel(equivalenceTable, labeledImage[r][c])
                 if label not in newTable:
                     newTable[label]       = len(newTable.keys()) + 1
                     Areas.append(0)
-                    Centeroids.append((0, 0))
-                labeldImage[r][c]       = newTable[label]
-                Areas[label-1]         += 1
-                Centeroids[label-1][0] += c
-                Centeroids[label-1][1] += r
+                    Centeroids.append([0, 0])
+                labeledImage[r][c]      = newTable[label]
+                Areas[newTable[label]-1]         += 1
+                Centeroids[newTable[label]-1][0] += c
+                Centeroids[newTable[label]-1][1] += r
     for i in range(len(newTable)):
         Centeroids[i][0] /= Areas[i]
         Centeroids[i][1] /= Areas[i]
-    return len(newTable), labeldImage, Areas, Centeroids
+    return len(newTable), labeledImage, Areas, Centeroids
 
 
 def getConnectedComponent(image: np.ndarray):   
-    labeldImage      = np.zeros_like(image)
+    labeledImage      = np.zeros_like(image)
     equivalenceTable = {}
     for r in range(image.shape[0]):
         for c in range(image.shape[1]):
             if image[r][c]:
-                neighbourLabels = getProirNeighbourLabels(labeldImage, r, c)
+                neighbourLabels = getProirNeighbourLabels(labeledImage, r, c)
                 if len(neighbourLabels) == 0:
                     newLabel = len(list(equivalenceTable.keys())) + 1
                     equivalenceTable[newLabel] = []
-                    labeldImage[r][c] = newLabel
+                    labeledImage[r][c] = newLabel
                 else:
                     label = getLabel(equivalenceTable, min(neighbourLabels))
-                    labeldImage[r][c] = label
+                    labeledImage[r][c] = label
                     for l in neighbourLabels:
                         if l != label:
                             fixEquivalenceTable(equivalenceTable, l, label)
-    return equalizeNormalizeLabeldImage(labeldImage, equivalenceTable)
+    return equalizeNormalizeLabeledImage(labeledImage, equivalenceTable)
 
 
 def getColors(numColors):
